b50a816bb6a8e70a4fcc43ccd07479bd222d12a3
racross1 <racross1@gmail.com>
2021-01-11 20:44:47 -0800

    commit tree cd529d376e2c4c7bee18a8db690bd833d6ab6330

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let arr = []
+    for (let i = 0; i < names.length; i++) {
+        arr[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    }
+    return arr
+}
+
+function countDown(int) {
+    let i = int
+    while (i > -1) {
+        console.log(i)
+        i--
+    }
+
+}
